15340c1179df70a5b0d20f2d54fd23304f68159d
amit <singhamit3738@gmail.com>
2023-02-20 17:13:59 +0530

    initial commit

---
+++ SpringBootLoginDetails1Application.java
@@ -0,0 +1,13 @@
+package com.logindetails1.springbootlogindetails1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootLoginDetails1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootLoginDetails1Application.class, args);
+	}
+
+}
---
+++ repository/LoginDetails1Repository.java
@@ -0,0 +1,7 @@
+package com.logindetails1.springbootlogindetails1.repository;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class LoginDetails1Repository{
+}
